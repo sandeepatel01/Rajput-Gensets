afe4894f69e85fa30947aca333a41372e9d7fd96
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-06 19:15:55 +0530

    feat: file upload

---
+++ backend/src/lib/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import { ApiError } from "../utils/ApiError.js";
+import path from "path/promises";
+import fs from "fs";
+
+cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+const cloudinaryUpload = async (localFilePath) => {
+      if (!localFilePath) {
+            throw new ApiError(400, "File not found");
+      };
+
+      try {
+            const response = await cloudinary.uploader.upload(localFilePath, {
+                  resource_type: "auto"
+            });
+
+            return response
+      } catch (error) {
+            throw new ApiError(400, error.message || "File upload failed");
+      } finally {
+            try {
+                  const filePath = path.resolve(localFilePath);
+                  await fs.unlink(filePath);
+            } catch (error) {
+                  console.error(`Error deleting local file: ${localFilePath}`, error);
+            }
+      }
+}
