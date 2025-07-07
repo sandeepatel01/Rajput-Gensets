7747fa13efffa52355120661926f389e2cc43a17
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-08 00:02:52 +0530

    feat: verifyEmail

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,14 @@
+import ms from "ms";
 import { cloudinaryUpload } from "../lib/cloudinary";
 import { sendVerificationMail } from "../lib/sendMail";
 import { User } from "../models/user.model";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
-import { generateToken, hashPassword } from "../utils/helper";
+import { generateAccessToken, generateRefreshToken, generateToken, hashToken } from "../utils/helper";
 import { sanitizeUser } from "../utils/sanitizeUser";
+import { Session } from "../models/session.model";
+import { generateCookieOptions } from "../lib/generateCookieOptions";
 
 const register = asyncHandler(async (req, res) => {
       const { fullname, email, password } = req.body;
@@ -54,6 +57,52 @@ const register = asyncHandler(async (req, res) => {
       res.status(201).json(new ApiResponse(201, "User registered successfully", safeUser));
 });
 
+const verifyEmail = asyncHandler(async (req, res) => {
+      const { token } = req.params;
+
+      if (!token) throw new ApiError(400, "Token not found");
+
+      const hashedToken = hashToken(token);
+
+      const user = await User.findOne({
+            verificationToken: hashedToken,
+            verificationTokenExpiry: { $gt: Date.now() }
+      });
+
+      if (!user) throw new ApiError(400, "Verification token is invalid or expired");
+
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
+
+      const hashedRefreshToken = hashToken(refreshToken);
+
+      const expiresAt = new Date(Date.now() + ms(process.env.REFRESH_TOKEN_EXPIRY));
+
+      await User.findByIdAndUpdate(user._id, {
+            isVerified: true,
+            verificationToken: null,
+            verificationTokenExpiry: null,
+      });
+
+      await Session.create({
+            userId: user._id,
+            userAgent: req.headers["user-agent"],
+            ipAddress: req.ip,
+            refreshToken: hashedRefreshToken,
+            expiresAt,
+      });
+
+      console.log("User verified successfully: ", { email: user.email, userId: user._id, IP: req.ip });
+
+      res
+            .status(200)
+            .cookie("accessToken", accessToken, generateCookieOptions())
+            .cookie("refreshToken", refreshToken, generateCookieOptions())
+            .json(new ApiResponse(200, "User verified successfully", null));
+
+});
+
 export {
-      register
+      register,
+      verifyEmail
 }
