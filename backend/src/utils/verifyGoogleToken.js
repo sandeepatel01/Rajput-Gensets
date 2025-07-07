dcbad2128a8a336d7789a2f0610ea3a9876e47eb
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-07 07:26:16 +0530

    feat: Google Login

---
+++ backend/src/utils/verifyGoogleToken.js
@@ -0,0 +1,28 @@
+import { OAuth2Client } from "google-auth-library";
+import { ApiError } from "./ApiError";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+export const verifyGoogleToken = async (token) => {
+      if (!token) {
+            throw new ApiError(400, "Google token not found");
+      }
+
+      let payload;
+      try {
+            const ticket = await client.verifyIdToken({
+                  idToken: token,
+                  audience: process.env.GOOGLE_CLIENT_ID,
+            });
+
+            payload = ticket.getPayload();
+      } catch (error) {
+            throw new ApiError(400, "Invalid Google token");
+      };
+
+      if (!payload) {
+            throw new ApiError(401, "Google token verification failed, playload not found");
+      };
+
+      return payload;
+}
