ecb1162f00fa07935990dee0f06d8843c4de246b
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-08 09:59:16 +0530

    feat: Production grade Authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import ms from "ms";
+import jwt from "jsonwebtoken";
 import { cloudinaryUpload } from "../lib/cloudinary";
 import { sendResetPasswordMail, sendVerificationMail } from "../lib/sendMail";
 import { User } from "../models/user.model";
@@ -10,9 +11,9 @@ import { sanitizeUser } from "../utils/sanitizeUser";
 import { Session } from "../models/session.model";
 import { generateCookieOptions } from "../lib/generateCookieOptions";
 import { zodErrorHandler } from "../utils/zodErrorHandler";
-import { validateEmail, validateLogin } from "../validators/authValidation";
-import { ht } from "date-fns/locale";
+import { validateEmail, validateLogin, validateResetPassword } from "../validators/authValidation";
 import { ProviderEnum } from "../utils/constants";
+import { sessionFormatter } from "../utils/sessionFormatter";
 
 const register = asyncHandler(async (req, res) => {
       const { fullname, email, password } = req.body;
@@ -257,11 +258,160 @@ const forgotPassword = asyncHandler(async (req, res) => {
             .json(new ApiResponse(200, "if you have an account with us, we will send you an email to forget your password", null));
 });
 
+const resetPassword = asyncHandler(async (req, res) => {
+      const { token } = req.params;
+      const { password } = zodErrorHandler(validateResetPassword(req.body));
+
+      if (!token) {
+            throw new ApiError(400, "Token not found");
+      };
+
+      const hashedToken = hashToken(token);
+      const user = await User.findOne({
+            resetPasswordToken: hashedToken,
+            resetPasswordTokenExpiry: { $gt: Date.now() }
+      });
+
+      if (!user) {
+            throw new ApiError(400, "Reset password token is invalid or expired");
+      };
+
+      const isPasswordValid = await comparePassword(password, user.password);
+      if (isPasswordValid) {
+            throw new ApiError(400, "New password must be different from the current password");
+      };
+
+      const hashedPassword = await hashPassword(password);
+
+      await User.findByIdAndUpdate(user._id, {
+            password: hashedPassword,
+            resetPasswordToken: null,
+            resetPasswordTokenExpiry: null
+      });
+
+      await Session.deleteMany({ userId: user._id });
+
+      console.log("Password reset successfully: ", { email: user.email, userId: user._id, IP: req.ip });
+
+      res
+            .status(200)
+            .json(new ApiResponse(200, "Password reset successfully", null));
+
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+      const incomingRefreshToken = req.cookies?.refreshToken;
+      if (!incomingRefreshToken) {
+            throw new ApiError(400, "Refresh token not found");
+      };
+
+      let decodedToken;
+      try {
+            decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+      } catch (error) {
+            throw new ApiError(400, "Invalid refresh token");
+      };
+
+      const hashedIncomingRefreshToken = hashToken(incomingRefreshToken);
+      const validToken = await Session.findOne({
+            refreshToken: hashedIncomingRefreshToken
+      });
+
+      if (!validToken) {
+            throw new ApiError(400, "Invalid refresh token");
+      };
+
+      const incomingUserAgent = req.headers["user-agent"];
+      const incomingIP = req.ip;
+
+      if (validToken.userAgent !== incomingUserAgent || validToken.ipAddress !== incomingIP) {
+            const result = await Session.deleteOne({ _id: validToken._id });
+            if (result.deletedCount === 0) {
+                  throw new ApiError(404, "Session not found");
+            }
+
+            throw new ApiError(400, "Session has expired. Please log in again.");
+      };
+
+      const accessToken = generateAccessToken(decodedToken);
+      const refreshToken = generateRefreshToken(decodedToken);
+
+      const hashedRefreshToken = hashToken(refreshToken);
+
+      await Session.findByIdAndUpdate(validToken.id, {
+            refreshToken: hashedRefreshToken,
+      });
+
+      console.log("Access token refreshed successfully");
+
+      res
+            .status(200)
+            .cookie("accessToken", accessToken, generateCookieOptions())
+            .cookie("refreshToken", refreshToken, generateCookieOptions({ rememberMe: validToken.keepSignedIn }))
+            .json(new ApiResponse(200, "Access token refreshed successfully", null));
+
+});
+
+const logoutAllSessions = asyncHandler(async (req, res) => {
+      const { id } = req.user;
+      const { refreshToken } = req.cookies;
+
+      if (!refreshToken) {
+            throw new ApiError(400, "Refresh token not found");
+      };
+
+      const hashedRefreshToken = hashToken(refreshToken);
+
+      await Session.deleteMany({
+            userId: id,
+            refreshToken: { $ne: hashedRefreshToken },
+      });
+
+      console.log("Logout all sessions");
+
+      res
+            .status(200)
+            .json(new ApiResponse(200, "Logout all sessions successfully", null));
+
+});
+
+const getActiveSessions = asyncHandler(async (req, res) => {
+      const { id: userId } = req.user;
+
+      const currentRefreshToken = req.cookies.refreshToken;
+      const hashedRefreshToken = hashToken(currentRefreshToken);
+
+      const sessions = await Session.find({ userId })
+            .select("id ipAddress userAgent updatedAt expiresAt refreshToken")
+            .sort({ createdAt: -1 })
+            .lean();
+
+      const setCurrentFlag = sessions.map(session => ({
+            ...session,
+            isCurrent: session.refreshToken === hashedRefreshToken
+      }));
+
+      const removeRefreshToken = setCurrentFlag.map(({
+            refreshToken,
+            ...rest
+      }) => rest);
+      const formattedSessions = await sessionFormatter(removeRefreshToken);
+
+      res
+            .status(200)
+            .json(new ApiResponse(200, "Get active sessions successfully", formattedSessions));
+
+});
+
 export {
       register,
       verifyEmail,
       resendVerificationMail,
       login,
       logout,
-      forgotPassword
+      forgotPassword,
+      resetPassword,
+      refreshAccessToken,
+      logoutAllSessions,
+      getActiveSessions
 }
