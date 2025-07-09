48cb9c94c08d55fba0987c73fcf68bcd2d9ca460
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-09 11:03:09 +0530

    feat: Tailwind setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <>
       <div>
-        <h1>Rajputs genset & Solar Services</h1>
+        <h1 className="font-bold text-green-700">
+          Rajputs genset & Solar Services
+        </h1>
       </div>
     </>
   );
