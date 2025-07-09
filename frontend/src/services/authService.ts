3568031d141688b548721a574facf6cccd1aadfb
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-09 14:37:01 +0530

    feat: User resentVerification mail service

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,11 @@
 import { AUTH_PATH } from "../constants";
 import axiosInstance from "../lib/axiosInstance";
-import type { ApiResponse, LoginFormData, User } from "../types";
+import type {
+  ApiResponse,
+  LoginFormData,
+  ResendVerificationFormData,
+  User,
+} from "../types";
 
 export const register = async (data: FormData) => {
   const res = await axiosInstance.post<ApiResponse<User>>(
@@ -32,6 +37,14 @@ export const googleLogin = async (data: {
 export const verifyEmail = async (token: string) => {
   const res = await axiosInstance.get<ApiResponse<null>>(
     `${AUTH_PATH}/verify/${token}`
+  );
+  return res.data;
+};
+
+export const resendVerification = async (data: ResendVerificationFormData) => {
+  const res = await axiosInstance.post<ApiResponse<null>>(
+    `${AUTH_PATH}/email/resend`,
+    data
   );
   return res.data;
 };
