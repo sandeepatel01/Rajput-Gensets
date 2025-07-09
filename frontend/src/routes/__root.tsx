70439fe37b138e2fc4d3637eb6339272b24a0d0b
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-09 17:35:14 +0530

    fix: file import

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 export const Route = createRootRoute({
   component: RootComponent,
