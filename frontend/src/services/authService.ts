732abb845bd2b63e7d4ef5ec17cb8c71ec8b1499
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-09 14:16:57 +0530

    feat: User login service

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,10 +1,18 @@
 import { AUTH_PATH } from "../constants";
 import axiosInstance from "../lib/axiosInstance";
-import type { ApiResponse, User } from "../types";
+import type { ApiResponse, LoginFormData, User } from "../types";
 
 export const registerUser = async (data: FormData) => {
   const res = await axiosInstance.post<ApiResponse<User>>(
     `${AUTH_PATH}/register`,
+    data
+  );
+  return res.data;
+};
+
+export const loginUser = async (data: LoginFormData) => {
+  const res = await axiosInstance.post<ApiResponse<null>>(
+    `${AUTH_PATH}/login`,
     data
   );
   return res.data;
