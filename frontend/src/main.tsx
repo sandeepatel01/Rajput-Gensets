fe96e04ab153bf5330431555560b65f775a61948
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-07-11 16:35:46 +0530

    feat: setup @react-oauth/google

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
 const router = createRouter({ routeTree });
 
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
